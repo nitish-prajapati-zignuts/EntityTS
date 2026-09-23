0c227ec4c6ab477b26434dc38ca48ba0c89fac45
nitish-prajapati-zignuts <nitishp@zignuts.com>
2026-09-23 12:59:56 +0530

    example: define Comment entity with foreign keys

---
+++ example/src/entities/Comment.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  Table,
+  PrimaryKey,
+  Column,
+  CreatedAt,
+  BelongsTo,
+  SqlType,
+} from '@nsp/dbcontext';
+import { Post } from './Post';
+
+@Entity()
+@Table('comments')
+export class Comment {
+  @PrimaryKey({ autoIncrement: true })
+  @Column({ type: SqlType.Int })
+  id!: number;
+
+  @Column({ name: 'post_id', type: SqlType.Int })
+  postId!: number;
+
+  @Column({ name: 'author', type: SqlType.VarChar, maxLength: 100 })
+  author!: string;
+
+  @Column({ name: 'text', type: SqlType.Text })
+  text!: string;
+
+  @CreatedAt()
+  createdAt!: Date;
+
+  @BelongsTo(() => Post, { foreignKey: 'post_id' })
+  post?: Post;
+}
