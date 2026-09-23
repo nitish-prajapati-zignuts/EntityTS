73dffed70ec52bf4a4b552efe63cf28c263d4484
nitish-prajapati-zignuts <nitishp@zignuts.com>
2026-09-23 12:59:55 +0530

    feat(procedure): define SqlType definitions for sproc parameters

---
+++ src/procedure/SqlType.ts
@@ -0,0 +1,38 @@
+export enum SqlType {
+  VarChar = 'VarChar',
+  NVarChar = 'NVarChar',
+  Text = 'Text',
+  NText = 'NText',
+  Char = 'Char',
+  NChar = 'NChar',
+
+  Int = 'Int',
+  BigInt = 'BigInt',
+  SmallInt = 'SmallInt',
+  TinyInt = 'TinyInt',
+  Decimal = 'Decimal',
+  Numeric = 'Numeric',
+  Float = 'Float',
+  Real = 'Real',
+  Money = 'Money',
+
+  Bit = 'Bit',
+  Boolean = 'Boolean',
+
+  Date = 'Date',
+  DateTime = 'DateTime',
+  DateTime2 = 'DateTime2',
+  SmallDateTime = 'SmallDateTime',
+  Time = 'Time',
+  Timestamp = 'Timestamp',
+  DateTimeOffset = 'DateTimeOffset',
+
+  Binary = 'Binary',
+  VarBinary = 'VarBinary',
+  Image = 'Image',
+
+  UniqueIdentifier = 'UniqueIdentifier',
+  Uuid = 'Uuid',
+  Json = 'Json',
+  Xml = 'Xml',
+}
